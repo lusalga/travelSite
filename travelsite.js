245db47e10ae8888c1b5c34940ab884e92a74c16
lusalga <lucieni.saldanha@gmail.com>
2020-03-09 09:39:38 -0500

    created and exported module fortune.js

--- travelsite.js
+++ travelsite.js
@@ -1,22 +1,12 @@
 // After installing Express by npm,
 // we load/import the Express module in main application file
 const express = require('express');  
+// import module created and located in lib folder
+const fortune = require('./lib/fortune.js');
 
 // create the app object, our Express application, by calling top-level 
 // express() function
 const app = express();
-
-// define an array of fortune cookies message to be displayed on the About page
-const fortunes = [
-    "Love is all that matters in the end.",
-    "Be a hero today!",
-    "A fresh start will put you on your way.",
-    "A friend asks only for your time not your money",
-    "The fortune you seek is in another cookie.",
-    "You will live long enough to open many fortune cookies.",
-    "You can always find happiness at work on Friday.",
-    "Fortune not found? Abort, Retry, Ignore.",
-]
 
 // set up express-handlebars view engine (template framework)
 // load the express-handlebars modules, create a default layout called main
@@ -40,9 +30,7 @@ app.get(['/','/home'],(req,res) => {
 });
 
 app.get(('/about'),(req,res) => {
-    const randomNumber = Math.floor(Math.random()*fortunes.length);
-    const randomFortune = fortunes[randomNumber];
-    res.render('about',{cookie: randomFortune});
+    res.render('about',{ cookie: fortune.getFortune() } );
 });
 
  // custom 404 page
@@ -55,7 +43,6 @@ app.use((req,res,next) => {
 
  // custom 500 page
  // 500 error handler(middleware)
-
  app.use((err,req,res, next) => {
     console.error(err.stack);
     res.status(500).render('500');
